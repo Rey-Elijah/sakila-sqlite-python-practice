222449fc19326983c03ca23a045a4be81fdb55c4
Rey-Elijah <reyelijahmoralesperez@gmail.com>
2026-01-18 11:55:22 -0600

    Muchos cambios
    - la ruta de la base de datos ahora es compartible con linux y otros os
    
    - algunas funciones repetitivas fueron combinadas
    
    - se incluyo la busqueda por actor

--- scripts/main_modular.py
+++ scripts/main_modular.py
@@ -14,9 +14,10 @@
 
 import sqlite3
 import os
+import textwrap
 # configuracion global
 DEBUGGIN = False # activa el modo debug que habilita mensajes e impide que no se limpie la terminal al usar el programa
-DB_PATH = "src/database/sqlite-sakila.db" # ruta relativa de la base de datos
+DB_PATH = r"src\database\sqlite-sakila.db" # ruta relativa de la base de datos
 
 # fnc para realizar la conexion de la base de datos parasada como argumento
 # - db_path: String el cual indica la ruta de la base de datos
@@ -33,40 +34,89 @@ def obtener_conexion(db_path):
 # de la pelicula ingresada por el usuario ya sea el nombre completo o parcial
 # conn: conexion de la base de datos
 # seach: String el cual se usara para realizar las comparaciones y obtener las coincidencias
-def seach_movies(conn, search):
+def do_query(conn, search, search_type):
     debug_print(f"Buscando coincidencias con: {search}...")
     cursor = conn.cursor()
-    query = '''
-                SELECT f.film_id
-                     , f.title
-                     , f.release_year
-                     , f.rating
-                     , GROUP_CONCAT(a.first_name || ' ' || a.last_name, ', ') AS actores
-                FROM film AS f
-                INNER JOIN film_actor AS fa
-                ON fa.film_id = f.film_id
-                INNER JOIN actor AS a
-                ON fa.actor_id = a.actor_id
-                WHERE f.title LIKE ?
-                GROUP BY f.film_id
-                LIMIT 5;
-                '''
+    match search_type:
+        case "by_film":            
+            query = '''
+                    SELECT f.film_id
+                         , f.title
+                         , f.release_year
+                         , f.rating
+                         , GROUP_CONCAT(a.first_name || ' ' || a.last_name, ', ') AS actores
+                    FROM film AS f
+                    INNER JOIN film_actor AS fa
+                    ON fa.film_id = f.film_id
+                    INNER JOIN actor AS a
+                    ON fa.actor_id = a.actor_id
+                    WHERE f.title LIKE ?
+                    GROUP BY f.film_id
+                    LIMIT 5;
+                    '''
+        case "by_actor":
+            query = '''
+                    SELECT a.actor_id
+                        , a.first_name
+                        , a.last_name
+                        , GROUP_CONCAT(f.title, ', ') AS films
+                    FROM actor AS a
+                    INNER JOIN film_actor AS fa
+                    ON fa.actor_id = a.actor_id
+                    INNER JOIN film as f
+                    ON f.film_id = fa.film_id
+                    WHERE (a.first_name || ' ' || a.last_name) LIKE ?
+                    GROUP BY a.actor_id
+                    LIMIT 5
+                    '''
     # ejecuta la consulta SQL y parasamos como segundo parametro el input como tupla
     cursor.execute(query, (f"%{search}%",))
     # obtenemos los resultados
     rows = cursor.fetchall()
     return rows
-# fnc para imprimir la lista de peliculas dada y darles formato
-# rows: lista de peliculas obtenidas de un query
-def print_movies(rows, busqueda):
+# fnc para imprimir el query del tipo de busqueda que haya realizado el usuario
+# rows: lista de datos obtenidas de un query
+def print_query(rows, busqueda, search_type):
     debug_print("Mostrando peliculas...")
     if rows:
-        print(f"\n{'ID':<5} | {'TITULO':<30} | {'ANIO'} | {'RATING'} | {'ACTOR'}")
-        print("-" * 60)
-        for row in rows:
-            print(f"{row['film_id']:<5} | {row['title']:<30} | {row['release_year']} | {row['rating']:<5} | {row['actores']}")
+        match search_type:
+            case "by_film":
+                print_films(rows)
+            case "by_actor":
+                print_actors(rows)
     else:
         print(f"No se encontraron peliculas que coincidan con: '{busqueda}'")
+# Fnc
+def print_films(rows):
+    # Encabezados por columna
+    print(f"\n{'ID':<5} | {'TITULO':<30} | {'ANIO'} | {'RATING'} | {'ACTOR'}")
+    print("-" * 60)
+    # Datos por fila
+    for row in rows:
+        print(f"{row['film_id']:<5} | {row['title']:<30} | {row['release_year']} | {row['rating']:<5} | {row['actores']}") 
+
+def print_actors(rows):
+    # Definimos los anchos para las columnas
+    id_w, fn_w, ln_w, films_w = 5, 12, 12, 50
+    # Definimos e imprimimos los encabezados de cada columna
+    header = f"\n{'ID':<{id_w}} | {'FIRST NAME':<{fn_w}} | {'LAST NAME':<{ln_w}} | {'films':<{films_w}}"
+    print(f"\n{header}")
+    print("-" * len(header))
+    
+    for row in rows:
+        # Envolvemos el texto de peliculas a 50 caracteres (films_w) para que no ocupe tanto espacio en consola
+        wrapped_films = textwrap.wrap(row['films'], width=films_w)
+        first_line_films = wrapped_films[0] if wrapped_films else ""
+        # Imprimimos junto con los demas valores de columnas
+        print(f"{row['actor_id']:<{id_w}} | {row['first_name']:<{fn_w}} | {row['last_name']:<{ln_w}} | {first_line_films}")
+        # Si hay mas lineas de peliculas, las imprimimos indentadas
+        for line in wrapped_films[1:]:
+            # Dejamos espacios vacios en las columnas de ID, Nombre y Apellido
+            indent = " " * (id_w + 3 + fn_w + 3 + ln_w + 1)
+            print(f"{indent}| {line}")
+        # Separador entre cada actor
+        print("-" * len(header))
+
 # fnc que muestra el menu se seleccion y que devuelve la opcion ingresada la cual es un int
 def option_selection():
     print('''
@@ -82,16 +132,19 @@ def option_selection():
     if opcion.isdigit():
         return int(opcion)
     return -1
+
 # fnc que ejecuta una de varias funciones dependiendo la seleccion del usuario pasada como parametro
 # - selection: int que se evalua para ejecutar la fnc
 def do_func(selection):
     match selection:
         case 1:
-            search_movie()
+            search("by_film")
             pause()
             clean_screen()
         case 2:
-            pass
+            search("by_actor")
+            pause()
+            clean_screen()
         case 3:
             pass
         case 4:
@@ -99,14 +152,22 @@ def do_func(selection):
         case _:
             print("error: OPCION INVALIDA")
 # fnc encargada de buscar y mostrar las peliculas apartir del texto que ingrese el usuario
-def search_movie():
-    # input del usuario para realizar la busqueda de peliculas
-    busqueda = input("Ingresa el nombre de la pelicula (o parte de el): ")
+def search(search_type):
+    match search_type:
+        case "by_film":
+            busqueda = input("Ingresa el nombre de la pelicula (o parte de el)...:")
+        case "by_actor":
+            busqueda = input("Ingresa el nombre del actor (o parte de el)...:")
+        case "show_top":
+            pass
+
     # obtenemos la conexion a la db en la ruta de la variable global DB_PATH
     mi_conexion = obtener_conexion(DB_PATH)
     if mi_conexion:
-        rows = seach_movies(mi_conexion, busqueda)
-        print_movies(rows, busqueda)
+        # Obtenemos los resultados del query
+        rows = do_query(mi_conexion, busqueda, search_type)
+        # Imprimimos el query en consola
+        print_query(rows, busqueda, search_type)
         mi_conexion.close()
 
 # fnc para imprimir mensajes debug en la consola cuando la variable 'DEBUGGIN' es True
@@ -114,8 +175,8 @@ def search_movie():
 def debug_print(message):
     if not (DEBUGGIN): return
     print(f"DEBUG: {message}")
-# fnc que limpia la terminal al ser llamada
 
+# fnc que limpia la terminal al ser llamada
 def clean_screen():
     if (DEBUGGIN): return
     # "nt" es para Windows, por defecto sera "clear" para otros sistemas
@@ -126,6 +187,7 @@ def pause(menssage = "\nPresiona <Enter> para continuar..."):
     input(menssage)
 
 def main():
+    # opcion del usuario
     opcion = -1
     clean_screen()
     # mientras la opcion no sea 4 no se acabara el programa
